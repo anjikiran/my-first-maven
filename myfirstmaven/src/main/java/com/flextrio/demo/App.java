5f2ec67b57221e779c359ed9d0da42cbfa90515c
anjikiran <anji.kiran@gmail.com>
2019-07-24 13:36:56 +0530

    my location

---
+++ myfirstmaven/src/main/java/com/flextrio/demo/App.java
@@ -0,0 +1,13 @@
+package com.flextrio.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
